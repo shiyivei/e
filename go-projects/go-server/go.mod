f76bb2b70cd17aee9a113e8539c3e170ceed27c8
shiyivei <shiyivei@outlook.com>
2022-06-10 00:23:58 +0800

    go-server

---
+++ go-projects/go-server/go.mod
@@ -0,0 +1,3 @@
+module go-projects/go-server
+
+go 1.17
---
+++ go-projects/go-server/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful")
+
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+
+	//print get content to user
+	fmt.Fprintf(w, "Name - %s\n", name)
+	fmt.Fprintf(w, "Address - %s\n", address)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "did not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "method not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "hello!")
+}
+
+func main() {
+
+	//read file from local filesystem
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+
+	//handle request
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+
+}
